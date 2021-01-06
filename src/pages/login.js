039fe8fd8051748fa308c7e8a3c209a5725cd58c
themrzlyv <themirzaliyev@gmail.com>
2021-01-06 22:22:31 +0400

    login page is styled and worked

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import cookie from 'js-cookie'
 import {useRouter} from 'next/router'
-
+import styles from '../styles/Login.module.scss'
 
 
 const Login = () => {
@@ -34,25 +34,44 @@ const Login = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input 
-                value={email}
-                type="text" 
-                onChange={(e)=> setemail(e.target.value)} 
-                name='email' 
-                placeholder='email'/>
-                <input 
-                value={password}
-                type="password" 
-                onChange={(e)=> setpassword(e.target.value)} 
-                name='password' 
-                placeholder='password'/>
-                <button 
-                type='submit'>
-                    Login now
-                </button>
-            </form>
+        <div className='container'>
+            <div className="row">
+                <div className="col-lg-8 mx-auto my-3 d-flex p-0 justify-content-center">
+                    <form className={styles.form} onSubmit={handleSubmit}>
+                        <div>
+                            <h4>
+                                <i className="far fa-envelope-open"></i>
+                                Email:
+                            </h4>
+                            <input 
+                            value={email}
+                            type="text" 
+                            onChange={(e)=> setemail(e.target.value)} 
+                            name='email' 
+                            placeholder='email'/>
+                        </div>
+                        <div>
+                            <h4>
+                                <i className="fas fa-passport"></i>
+                                Password:
+                            </h4>
+                            <input
+                            value={password} 
+                            type="password" 
+                            onChange={(e)=> setpassword(e.target.value)} 
+                            name='password' 
+                            placeholder='password'/>
+                        </div>
+                        <div>
+                            <button 
+                            type='submit'>
+                                Login
+                                <i className="far fa-check-circle"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
